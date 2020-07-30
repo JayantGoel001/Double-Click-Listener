be412f6ba5bdedc2ea08fb828b01bd3a7fd06fb8
CormacKrum <jgoel92@gmail.com>
2020-07-31 01:45:52 +0530

    Double Click Listener

--- DoubleClickListner/src/main/java/com/example/doubleclicklistner/DoubleClickListener.kt
+++ DoubleClickListner/src/main/java/com/example/doubleclicklistner/DoubleClickListener.kt
@@ -1,4 +1,34 @@
 package com.example.doubleclicklistner
 
-class DoubleClickListener {
+import android.view.View
+
+class DoubleClickListener(private val callback: Callback,private val doubleClickTimeLimit:Long=1000) :
+    View.OnClickListener {
+    private var lastClicked:Long = -1L
+    override fun onClick(v: View?) {
+        lastClicked = when{
+            lastClicked == -1L ->{
+                System.currentTimeMillis()
+            }
+            isDoubleClicked()->{
+                callback.doubleClicked()
+                -1L
+            }
+            else->{
+                System.currentTimeMillis()
+            }
+
+        }
+    }
+    private fun getTimeDifference(from:Long,to:Long):Long{
+        return  to-from
+    }
+    private fun isDoubleClicked():Boolean{
+        return getTimeDifference(lastClicked,System.currentTimeMillis())<=doubleClickTimeLimit
+    }
+    interface Callback{
+        fun doubleClicked()
+    }
+
+
 }
